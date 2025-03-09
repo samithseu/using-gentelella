4027d7f5886b0458d4996f613650f3ec6f5e4395
samithseu <samithseu2002@gmail.com>
2025-03-09 17:30:24 +0700

    updated `font-family`

--- drag-and-drop/v2/tailwind.config.js
+++ drag-and-drop/v2/tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   content: ["./index.html", "./script.js", "./style.css"],
   theme: {
+    fontFamily: {
+      sans: ["Kantumruy Pro", "sans-serif"],
+    },
     extend: {},
   },
 };
